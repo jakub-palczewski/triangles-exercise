07ec859db37532c12d5181d853d9c242968069bf
Jakub Palczewski <jakub.palczewski@invia.com>
2025-07-11 11:35:25 +0200

    Position.scala: * create model for Position * position represents location of element in the triangle

---
+++ src/main/scala/triangles/exercise/model/Position.scala
@@ -0,0 +1,9 @@
+package triangles.exercise.model
+
+
+/** Represents a position in the triangle
+ *
+ * @param row 0-based row index
+ * @param col 0-based column index within the row
+ */
+case class Position(row: Int, col: Int)
