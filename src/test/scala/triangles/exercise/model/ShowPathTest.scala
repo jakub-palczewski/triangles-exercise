4b0770b50f96e607cecaeb40847fbb3695a6357f
Jakub Palczewski <jakub.palczewski@invia.com>
2025-07-11 16:57:30 +0200

    ShowPathTest: * use vector in test

--- src/test/scala/triangles/exercise/model/ShowPathTest.scala
+++ src/test/scala/triangles/exercise/model/ShowPathTest.scala
@@ -6,11 +6,7 @@ import munit.FunSuite
 class ShowPathTest extends FunSuite:
   test("Show[Path] should format path correctly") {
     val path = Path(
-      Vector(
-        Node(Position(0, 0), 1),
-        Node(Position(1, 0), 2),
-        Node(Position(2, 0), 3)
-      )
+      Vector(1, 2, 3)
     )
     val result = Show[Path].show(path)
     assertEquals(result, "1 + 2 + 3 = 6")
