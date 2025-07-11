793210fa638b500a3f2dd1cf381472592a8d217f
Jakub Palczewski <jakub.palczewski@invia.com>
2025-07-11 17:03:20 +0200

    StreamPathFinder: * use all components and return result in main

--- src/main/scala/triangles/exercise/App.scala
+++ src/main/scala/triangles/exercise/App.scala
@@ -5,18 +5,19 @@ import cats.effect.std.Console
 import cats.effect.{ExitCode, IO, IOApp}
 import triangles.exercise.model.Path
 import triangles.exercise.service.factory.TriangleFactory
-import triangles.exercise.service.pathfinder.DummyPathFinder
+import triangles.exercise.service.pathfinder.{PathFinderImpl, StreamPathFinder}
 import triangles.exercise.service.reader.ConsoleReader
 
 object App extends IOApp.Simple:
 
-  private val consoleReader   = new ConsoleReader(Console[IO])
-  private val dummyPathFinder = new DummyPathFinder()
+  private val consoleReader    = new ConsoleReader(Console[IO])
+  private val simplePathFinder = new PathFinderImpl()
+  private val streamPathFinder = new StreamPathFinder()
 
   override def run: IO[Unit] =
     for
       input    <- consoleReader.readInput
       triangle <- IO.fromEither(TriangleFactory.fromRows(input))
-      minPath = dummyPathFinder.findMinPath(triangle)
-      _ <- Console[IO].println(s"Minimal path is: ${Show[Path].show(minPath)}")
+      minPath  <- streamPathFinder.findMinPath(triangle)
+      _        <- Console[IO].println(s"Minimal path is: ${Show[Path].show(minPath)}")
     yield ExitCode.Success
