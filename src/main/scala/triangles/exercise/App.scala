46960bef0af93bc8ff485bee8db3af89c699a21f
Jakub Palczewski <jakub.palczewski@invia.com>
2025-07-11 12:47:57 +0200

    App * run ConsoleReader from main for testing

--- src/main/scala/triangles/exercise/App.scala
+++ src/main/scala/triangles/exercise/App.scala
@@ -2,9 +2,14 @@ package triangles.exercise
 
 import cats.effect.std.Console
 import cats.effect.{ExitCode, IO, IOApp}
+import triangles.exercise.service.ConsoleReader
 
 object App extends IOApp.Simple:
 
+  private val consoleReader = new ConsoleReader(Console[IO])
+
   override def run: IO[Unit] =
-    for _ <- Console[IO].println("Hello, World!")
+    for
+      input <- consoleReader.readInput
+      _     <- Console[IO].print(input)
     yield ExitCode.Success
