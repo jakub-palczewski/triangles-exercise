cd49885bd97e9d857d2b131e970feb38727b8b2d
Jakub Palczewski <jakub.palczewski@invia.com>
2025-07-11 11:36:08 +0200

    Node.scala: * create Node model * model represents a node with element in triangle -position and its value

---
+++ src/main/scala/triangles/exercise/model/Node.scala
@@ -0,0 +1,8 @@
+package triangles.exercise.model
+
+/** Represents a node in the triangle
+ *
+ * @param position The position of this node
+ * @param value    The integer value at this position
+ */
+case class Node(position: Position, value: Int)
