bf98ac410feb00c0b09d01559ed713f5a6e05e75
Jakub Palczewski <jakub.palczewski@invia.com>
2025-07-11 14:33:56 +0200

    Path.scala * define Show[Path] * test Show instance

--- src/main/scala/triangles/exercise/model/Path.scala
+++ src/main/scala/triangles/exercise/model/Path.scala
@@ -1,3 +1,13 @@
 package triangles.exercise.model
 
+import cats.Show
+
 case class Path(nodes: List[Node])
+
+object Path:
+  given Show[Path] = Show.show[Path] { path =>
+    val values = path.nodes.map(_.value)
+    val sum    = values.sum
+
+    s"${values.mkString(" + ")} = $sum"
+  }
---
+++ src/test/scala/triangles/exercise/model/ShowPathTest.scala
@@ -0,0 +1,17 @@
+package triangles.exercise.model
+
+import cats.Show
+import munit.FunSuite
+
+class ShowPathTest extends FunSuite:
+  test("Show[Path] should format path correctly") {
+    val path = Path(
+      List(
+        Node(Position(0, 0), 1),
+        Node(Position(1, 0), 2),
+        Node(Position(2, 0), 3)
+      )
+    )
+    val result = Show[Path].show(path)
+    assertEquals(result, "1 + 2 + 3 = 6")
+  }
