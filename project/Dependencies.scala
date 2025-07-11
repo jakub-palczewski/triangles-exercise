e3e04a1f5be508ed536a4bef40083dcb0161467d
Jakub Palczewski <jakub.palczewski@invia.com>
2025-07-11 16:55:58 +0200

    Dependencies: * add fs2 dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,12 +4,14 @@ object Dependencies {
 
   val Versions = new {
     val catsEffectVersion = "3.6.2"
+    val fs2Version = "3.12.0"
     val mUnitVersion = "1.1.1"
     val mUnitCatsEffectVersion = "1.0.7"
   }
 
   val dependencies = Seq(
     "org.typelevel" %% "cats-effect" % Versions.catsEffectVersion,
+    "co.fs2" %% "fs2-core" % Versions.fs2Version,
     "org.scalameta" %% "munit" % Versions.mUnitVersion % Test,
     "org.typelevel" %% "munit-cats-effect-3" % Versions.mUnitCatsEffectVersion % Test
   )
