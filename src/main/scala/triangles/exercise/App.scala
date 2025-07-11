baf5ffc46e693a76e30b22d4d3ead29d518349d5
Jakub Palczewski <jakub.palczewski@invia.com>
2025-07-11 14:34:21 +0200

    App.scala: * run app flow using dummy implementation of algorithm

--- src/main/scala/triangles/exercise/App.scala
+++ src/main/scala/triangles/exercise/App.scala
@@ -1,15 +1,22 @@
 package triangles.exercise
 
+import cats.Show
 import cats.effect.std.Console
 import cats.effect.{ExitCode, IO, IOApp}
-import triangles.exercise.service.ConsoleReader
+import triangles.exercise.model.Path
+import triangles.exercise.service.factory.TriangleFactory
+import triangles.exercise.service.pathfinder.DummyPathFinder
+import triangles.exercise.service.reader.ConsoleReader
 
 object App extends IOApp.Simple:
 
-  private val consoleReader = new ConsoleReader(Console[IO])
+  private val consoleReader   = new ConsoleReader(Console[IO])
+  private val dummyPathFinder = new DummyPathFinder()
 
   override def run: IO[Unit] =
     for
-      input <- consoleReader.readInput
-      _     <- Console[IO].print(input)
+      input    <- consoleReader.readInput
+      triangle <- IO.fromEither(TriangleFactory.fromRows(input))
+      minPath = dummyPathFinder.findMinPath(triangle)
+      _ <- Console[IO].println(s"Minimal path is: ${Show[Path].show(minPath)}")
     yield ExitCode.Success
